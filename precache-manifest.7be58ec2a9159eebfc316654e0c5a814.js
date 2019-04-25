fa859ad6784c7c437fd75bb3c6a8e9b95e8f8b87
dionisiatara <taradionisia@yahoo.com>
2019-04-25 12:51:11 -0400

    Updates

---
+++ precache-manifest.7be58ec2a9159eebfc316654e0c5a814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec6a4bd07300fc2b8ca5",
+    "url": "/WhackAMoleGame/static/css/main.6e7362df.chunk.css"
+  },
+  {
+    "revision": "ec6a4bd07300fc2b8ca5",
+    "url": "/WhackAMoleGame/static/js/main.6d278243.chunk.js"
+  },
+  {
+    "revision": "76dfa203e60c83957ae7",
+    "url": "/WhackAMoleGame/static/js/runtime~main.5aed4600.js"
+  },
+  {
+    "revision": "ebcd1559c86e487caff9",
+    "url": "/WhackAMoleGame/static/js/2.f01f3e1e.chunk.js"
+  },
+  {
+    "revision": "b01fcf84c584b5df983bda79e1ef2f01",
+    "url": "/WhackAMoleGame/static/media/mole.b01fcf84.png"
+  },
+  {
+    "revision": "d6c13862b2cc548605ec4976c2e8b682",
+    "url": "/WhackAMoleGame/static/media/brown-hole.d6c13862.png"
+  },
+  {
+    "revision": "e7d9bfe086c8f1be49d23ccf098be42a",
+    "url": "/WhackAMoleGame/index.html"
+  }
+];
